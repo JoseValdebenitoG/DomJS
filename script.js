1a24d4f2d1c8e7e65bb96176d79a39057f0f28a9
Jose <jvaldebenitog@me.com>
2022-11-13 20:43:31 -0300

    clase 03 completada

--- script.js
+++ script.js
@@ -15,8 +15,28 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault(); // con esto se previene el refresco de la pagina
     // creamos variable input para traer los datos.
-    const input = document.querySelector("[data-form-input]"); 
-    console.log(input.value);
+    const input = document.querySelector("[data-form-input]");
+    // guardamos el contenido del input en una variable
+    const value = input.value;
+    // llamamos al elemento html
+    const list = document.querySelector("[data-list]")
+    // Creamos un elemento a la lista
+    const task = document.createElement('li');
+    // agregamos una clase al elemento li
+    task.classList.add('card');
+    input.value = "";
+    // Creamos un Template String
+    // y con ${} ponemos una variable js dentro del html
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    // agregamos el html al elemento task
+    task.innerHTML = content;
+    // agregamos un elemento hijo a la lista
+    list.appendChild(task);
+    console.log(content);
 }
 
 console.log(btn);
